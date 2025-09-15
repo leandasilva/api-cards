e3c4a6958e785518f0bc0ad07e28584fb8e5cee1
Leandro <leandro_delmillo@hotmail.com>
2025-09-15 19:29:30 -0300

    last commit 15/9

--- jest.setup.js
+++
@@ -1,9 +0,0 @@
-require('cross-fetch/polyfill');
-require('@testing-library/jest-dom');
-
-const { TextEncoder, TextDecoder } = require('util');
-global.TextEncoder = TextEncoder;
-global.TextDecoder = TextDecoder;
-
-
-
